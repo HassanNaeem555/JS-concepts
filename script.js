74721376582fcd7857d0e77729e8302d9bc84724
HassanNaeem555 <hassan.naeem@technado.co>
2022-01-07 04:54:22 +0500

    customise the console log

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function calculateTax(price, tax, description) {
   tax = tax ?? 0.05;
   description = description ?? "Default Item";
   const total = price * (1 + tax);
-  console.log(`${description} With Tax: $${total}`);
+  console.log(
+    `%c${description} With Tax: $${total}`,
+    "font-weight:900;color:#fff;background-color:gray;padding:15px;"
+  );
 }
 calculateTax(100, 2, "My item");
 calculateTax(100, 0, "My other item");
